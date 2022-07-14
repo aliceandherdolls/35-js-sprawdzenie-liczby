29daed76ef557677b07f284bf2e885a9d4f463d2
aliceandherdolls <alicjagorkiewicz92@gmail.com>
2022-07-14 16:32:17 +0200

    first

---
+++ js/main.js
@@ -0,0 +1,7 @@
+let number = 7;
+
+if (100 < number < 200) {
+    console.log("Liczba znajduje się w przedziale");
+} else if (100 > number > 200) {
+    console.log("Liczba nie znajduje się w przedziale");
+} 
